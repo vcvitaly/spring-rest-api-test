76493ece8203535f7287a29da2b9b9d0c0c99bb5
Vitalii Chura <vcvitaly@yahoo.com>
2024-09-14 04:04:59 +0300

    Initial commit

---
+++ src/main/java/io/github/vcvitaly/restapitest/RestApiTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.vcvitaly.restapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/vcvitaly/restapitest/RestApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vcvitaly.restapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
