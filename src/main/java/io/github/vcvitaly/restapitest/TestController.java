201b64c506488bd7454b05dbc17659662c35644c
Vitalii Chura <vcvitaly@yahoo.com>
2024-09-14 15:28:17 +0300

    add k6 test and fjp endpoint

--- src/main/java/io/github/vcvitaly/restapitest/TestController.java
+++ src/main/java/io/github/vcvitaly/restapitest/TestController.java
@@ -1,7 +1,12 @@
 package io.github.vcvitaly.restapitest;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    private final Map<Long, Object> map = new ConcurrentHashMap<>();
+    private final ExecutorService executorService = Executors.newFixedThreadPool(4);
 
     @GetMapping("/hello")
-    public String helloWorld() {
-        map.put(System.currentTimeMillis(), new Object());
-        return String.valueOf(map.size());
+    public CompletableFuture<String> helloWorld() {
+        return CompletableFuture.supplyAsync(() -> {
+            final String s = String.valueOf(get());
+            log.info("Got {} from get()", s);
+            return s;
+        });
+    }
+
+    @GetMapping("/hello-parallel")
+    public String helloWorldParallel() {
+        try (ForkJoinPool fjp = new ForkJoinPool(4)) {
+            return fjp.submit(
+                    () -> IntStream.range(0, 12).parallel().mapToObj(this::intToString).collect(Collectors.joining(","))
+            ).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private long get() {
@@ -25,5 +44,15 @@ public class TestController {
             throw new RuntimeException(e);
         }
         return System.currentTimeMillis();
+    }
+
+    private String intToString(int i) {
+        try {
+            Thread.sleep(1_000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("Converting {} to string on a thread {}", i, Thread.currentThread().getName());
+        return String.valueOf(i);
     }
 }
