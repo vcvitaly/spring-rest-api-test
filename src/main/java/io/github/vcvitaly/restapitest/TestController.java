74768f2de25236edb366db862691530b7a0528f9
Vitalii Chura <vitalii.chura@ge.com>
2024-11-29 16:53:06 +0200

    bump vers, add Flux endpoint

--- src/main/java/io/github/vcvitaly/restapitest/TestController.java
+++ src/main/java/io/github/vcvitaly/restapitest/TestController.java
@@ -1,5 +1,8 @@
 package io.github.vcvitaly.restapitest;
 
+import java.time.Duration;
+import java.util.Comparator;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
@@ -7,11 +10,19 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.retry.Retry;
 
 @Slf4j
 @RestController
 public class TestController {
+
+    private final Random random = new Random();
+    private final int concurrency = Runtime.getRuntime().availableProcessors() * 2;
 
     @GetMapping("/hello")
     public CompletableFuture<String> helloWorld() {
@@ -35,6 +46,24 @@ public class TestController {
         }
     }
 
+    @GetMapping("/hello-flux/{limit}")
+    public Mono<String> helloFlux(@PathVariable Integer limit) {
+        return Flux.fromStream(IntStream.rangeClosed(1, limit).boxed())
+                .map(i -> processInt(i, limit))
+                .retryWhen(
+                        Retry
+                                .backoff(3, Duration.ofSeconds(5))
+                                .maxBackoff(Duration.ofMinutes(5))
+                                .jitter(0.3)
+                                .doBeforeRetry(signal -> log.warn("load assets: retrying attempt {}", signal.totalRetries() + 1))
+                )
+                .doOnError(t -> log.error("Error while processing ints: ", t))
+                .parallel(concurrency)
+                .runOn(Schedulers.newParallel("int-processing", concurrency))
+                .collectSortedList(Comparator.comparingInt(Integer::intValue))
+                .map(list -> String.valueOf(list.stream().mapToInt(Integer::intValue).sum()));
+    }
+
     private long get() {
         try {
             Thread.sleep(1_000);
@@ -52,5 +81,17 @@ public class TestController {
         }
         log.info("Converting {} to string on a thread {}", i, Thread.currentThread().getName());
         return String.valueOf(i);
+    }
+
+    private int processInt(int i, int limit) {
+        if (random.nextInt(limit) == 0) {
+            throw new RuntimeException("Oops");
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return i;
     }
 }
